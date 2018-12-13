345e07bede400554017343610f038e6f62ef48f9
Taras Boreyko <tarasyo7@gmail.com>
2018-12-13 03:43:04 +0000

    Adding includes folder

---
+++ private/models/admin/includes/admin_footer.php
@@ -0,0 +1,12 @@
+<?php
+?>
+<!-- jQuery -->
+<script src="./../templates/js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="./../templates/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
+
